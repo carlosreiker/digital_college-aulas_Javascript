589d6d1f2d0ed3e4d4e633ef732a76526941092d
Carlos Reiker <reiker.santos@outlook.com>
2025-11-13 17:29:46 +0000

    Substitui logs por alertas nas mensagens de acerto e erro no sorteio de números

--- sort-numbers.js
+++ sort-numbers.js
@@ -2,7 +2,7 @@ let drawnNumber = Math.floor(Math.random() * 10);
 let chosenNumber = Number(prompt("Choose a number between 0 and 9:"));
 
 if(chosenNumber === drawnNumber) {
-    console.log(`Você acertou! O número sorteado foi ${drawnNumber}`);
+    alert(`Você acertou! O número sorteado foi ${drawnNumber}`);
 } else {
-    console.log(`Você errou! O número sorteado foi ${drawnNumber}`);
+    alert(`Você errou! O número sorteado foi ${drawnNumber}`);
 }
